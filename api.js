b6dcf0e37e17b2f16a0efadb3691fbfa218ab42c
tripott <tripott@gmail.com>
2018-02-01 15:51:50 -0500

    POST /todos with schema check and error handling

--- api.js
+++ api.js
@@ -2,8 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const search = require('./lib/to-do-search')
+const reqFieldChecker = require('./lib/check-req-fields')
+const objClean = require('./lib/clean-object')
+const HTTPError = require('node-http-error')
+
 const {
   propOr,
+  add,
+  sort,
+  map,
   compose,
   contains,
   head,
@@ -11,10 +18,14 @@ const {
   split,
   prop,
   pathOr,
-  filter
+  filter,
+  merge,
+  not,
+  isEmpty,
+  join
 } = require('ramda')
 
-//const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
 const port = propOr(9999, 'PORT', process.env)
 
 console.log('process.env.PORT', process.env.PORT)
@@ -26,7 +37,38 @@ const todos = [
   { id: 4, text: 'snore', completed: false }
 ]
 
+const todoRequiredFieldChecker = reqFieldChecker(['text'])
+
+/*
+req.params
+req.body
+req.query
+*/
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => res.send('<h1>Welcome to the ToDos API</h1>'))
+
+app.post('/todos', (req, res, next) => {
+  const sorter = (a, b) => a - b
+  const newID = compose(add(1), last, sort(sorter), map(todo => todo.id))(todos)
+
+  const missingFields = todoRequiredFieldChecker(req.body)
+
+  if (not(isEmpty(missingFields))) {
+    next(
+      new HTTPError(
+        400,
+        `Missing Fields: ${join(' ', todoRequiredFieldChecker(req.body))}`
+      )
+    )
+  }
+
+  todos.push(merge(req.body, { id: newID }))
+  res.status(201).send(merge(req.body, { id: newID }))
+  return
+})
+
 app.get('/todos', (req, res) => {
   if (pathOr(null, ['query', 's'], req)) {
     const searchProp = compose(head, split(':'), prop('s'))(req.query)
@@ -36,6 +78,11 @@ app.get('/todos', (req, res) => {
     res.send(todos)
   }
   return
+})
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500) // or use err.statusCode instead
+  res.send(err.message)
 })
 
 app.listen(port, () => console.log('TODOS API IS UP on port', port))

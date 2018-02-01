c15ec9df1d26c0b6d4f686bf44fad4b5b729f330
tripott <tripott@gmail.com>
2018-02-01 11:32:41 -0500

    added basic text prop search

--- api.js
+++ api.js
@@ -1,7 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const { propOr } = require('ramda')
+const {
+  propOr,
+  compose,
+  contains,
+  head,
+  last,
+  split,
+  prop,
+  pathOr,
+  filter
+} = require('ramda')
 
 //const port = process.env.PORT || 4000
 const port = propOr(9999, 'PORT', process.env)
@@ -15,6 +25,35 @@ const todos = [
 ]
 
 app.get('/', (req, res) => res.send('<h1>Welcome to the ToDos API</h1>'))
-app.get('/todos', (req, res) => res.send(todos))
+app.get('/todos', (req, res) => {
+  // GET /todos?s=text:teach
+  // req.query.s   // => `text:teach`
+
+  const isQuery = pathOr(null, ['query', 's'], req)
+
+  if (isQuery) {
+    const searchProp = compose(head, split(':'), prop('s'))(req.query)
+    const searchValue = compose(last, split(':'), prop('s'))(req.query)
+
+    //searchProp  // => 'text'
+    //searchValue // => 'teach'
+    console.log('searchProp', searchProp)
+    console.log('searchValue', searchValue)
+
+    var searchedToDos = []
+
+    function search(todo) {
+      return compose(contains(searchValue), split(' '))(todo[searchProp])
+    }
+
+    searchedToDos = filter(search, todos)
+
+    res.send(searchedToDos)
+  } else {
+    res.send(todos)
+  }
+
+  return
+})
 
 app.listen(port, () => console.log('TODOS API IS UP on port', port))
